86cb0be5df060799cc368525bebdb849f1c44b5e
DaffaXD <79232562+DaffaXD@users.noreply.github.com>
2021-02-18 15:06:39 +0700

    Update EternalAquaBgm.py
    will keep opening tabs with aqua bgm in it for etenity. if that isn't pain i dont know what is

--- TrollingNonSimps/EternalAquaBgm.py
+++ TrollingNonSimps/EternalAquaBgm.py
@@ -3,6 +3,6 @@ import webbrowser
 while True:
     #will loop for eternity until he/she presses Ctrl + C (which they cant because it will keep opening another tabs >:) )
     webbrowser.open('https://www.youtube.com/watch?v=kXma6hOqUIE')
-    #Baqua BGM for lyfe h3h3h3hh3h3
+    #Baqua BGM for lyfe h3h3h3hh3h
     
     #DaffaXD
