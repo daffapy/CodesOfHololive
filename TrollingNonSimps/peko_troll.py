215b96a7888601b23237724eeb08117a0254d82a
DaffaXD <79232562+DaffaXD@users.noreply.github.com>
2021-02-18 17:17:07 +0700

    Update peko_troll.py

--- TrollingNonSimps/peko_troll.py
+++ TrollingNonSimps/peko_troll.py
@@ -8,8 +8,10 @@ from pynput.keyboard import Listener , Key
 def on_press(key):
     if key == Key.space: #if 'space' was pressed,
         pyautogui.typewrite('peko') #then write 'peko' after it
+        #example : a -> a peko (adding peko automatically)
     elif key == Key.backspace: #if you try to delete it,
         pyautogui.typewrite('peko')#it will write more 'peko' hahahaha get rekt peko....
+        #example : a pek -> a pekpeko (adding peko automatically therefore you cant delete the peko madness)
 
 #To monitor the keyboard
 with Listener(on_press=on_press) as listener:
