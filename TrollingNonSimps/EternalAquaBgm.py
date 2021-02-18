11db11d0f5f5c2983577f56ac8a39dcdddc302b5
DaffaXD <79232562+DaffaXD@users.noreply.github.com>
2021-02-18 13:50:09 +0700

    Update EternalAquaBgm.py

--- TrollingNonSimps/EternalAquaBgm.py
+++ TrollingNonSimps/EternalAquaBgm.py
@@ -4,3 +4,5 @@ while True:
     #will loop for eternity until he/she presses Ctrl + C (which they cant because it will keep opening another tabs >:) )
     webbrowser.open('https://www.youtube.com/watch?v=kXma6hOqUIE')
     #Baqua BGM for lyfe h3h3h3hh3h3
+    
+    #DaffaXD
