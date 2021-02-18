fe9a9e9d6b5dfd50bbd558588e2910e8e0bea03f
DaffaXD <79232562+DaffaXD@users.noreply.github.com>
2021-02-18 12:02:21 +0700

    Update test_output_url.py

--- AutoTakeUrl/test_output_url.py
+++ AutoTakeUrl/test_output_url.py
@@ -4,12 +4,15 @@ import webbrowser
 import cv2
 import os
 
+#your previous .txt files that contains the links
 filepath = 'url_collector.txt'
 time.sleep(2)
 
+#opening sites
 webbrowser.open('http://www.get-youtube-thumbnail.com/')
 time.sleep(5)
 
+#reading input from your .txt files
 with open(filepath) as fp:
    #reading line by line
    line = fp.readline()
@@ -21,6 +24,7 @@ with open(filepath) as fp:
        #Locating search bar
        time.sleep(2)
        if i == 0: #i want it only to work once
+           #Clicking the search bar (use YOUR OWN path of the file, dont follow my path if you dont have same path/directory)
            img = os.path.expanduser('~/Documents/search_bar_url.png')
            click_search = pyautogui.locateCenterOnScreen(img,grayscale=True,confidence=.9)
            pyautogui.moveTo(click_search.x/2,click_search.y/2)
@@ -29,17 +33,18 @@ with open(filepath) as fp:
        #write
        pyautogui.typewrite(url)
        line = fp.readline()
-       #Getting thumbnail
+       #Getting the thumbnail (use YOUR OWN path of the file, dont follow my path if you dont have same path/directory)
        img_2 = os.path.expanduser('~/Documents/get_thumbnail.png')
        click_get = pyautogui.locateCenterOnScreen(img_2,grayscale=True,confidence=.9)
        pyautogui.moveTo(click_get.x/2,click_get.y/2)
        pyautogui.click()
-       #save or copy the image by yourself since im not using selenium, because im lazy.
+       #SAVE OR COPY IMAGE BY YOURSELF since im not using selenium,8 because im lazy.
+       #You can leave it again after copying or saving the file since code below will take care of that
        time.sleep(15)
        #scroll up
        pyautogui.scroll(50)
        time.sleep(2)
-       #delete the previous links
+       #delete the previous links (use YOUR OWN path of the file, dont follow my path if you dont have same path/directory)
        used_bar = os.path.expanduser('~/Documents/used_search_bar.png')
        delete_search = pyautogui.locateCenterOnScreen(used_bar,grayscale=True,confidence=.9)
        pyautogui.moveTo(delete_search.x/2,delete_search.y/2)
