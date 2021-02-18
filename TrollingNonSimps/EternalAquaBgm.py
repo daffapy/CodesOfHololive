d86066639367806b5bc7478bdc2a9d4c4911c86e
DaffaXD <79232562+DaffaXD@users.noreply.github.com>
2021-02-18 13:46:01 +0700

    Add files via upload

---
+++ TrollingNonSimps/EternalAquaBgm.py
@@ -0,0 +1,5 @@
+import webbrowser
+
+while True:
+    #will loop for eternity until he/she presses Ctrl + C (which they cant because it will keep opening another tabs >:) )
+    webbrowser.open('https://www.youtube.com/watch?v=kXma6hOqUIE')
