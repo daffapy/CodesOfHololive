57bd917d261d10c1d0f9c01ff759ef95d42269e3
DaffaXD <79232562+DaffaXD@users.noreply.github.com>
2021-02-19 08:23:58 +0700

    Update peko_troll.py

--- TrollingNonSimps/peko_troll.py
+++ TrollingNonSimps/peko_troll.py
@@ -2,7 +2,7 @@ import pyautogui
 import pynput
 from pynput.keyboard import Listener , Key
 
-#Purpose: To troll your friends so when they type 'space' it will automatically adding 'peko' after it and if they try to delete it, it will add more 'peko'
+#Purpose: To troll your friends so when they press 'space' it will automatically adding 'peko' after it and if they try to delete it, it will add more 'peko'
 
 #function for pressing key
 def on_press(key):
