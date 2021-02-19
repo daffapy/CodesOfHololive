804d2ca16c681923a4c2110c5ae4121fcb98d4b5
DaffaXD <79232562+DaffaXD@users.noreply.github.com>
2021-02-19 07:07:46 +0700

    Update and rename EternalAquaBgm.py to EternalAquaOP.py

--- TrollingNonSimps/EternalAquaOP.py
+++ TrollingNonSimps/EternalAquaOP.py
@@ -1,6 +1,6 @@
 import webbrowser
 
-#Purpose: To make as many tabs as your friend's computer can handle with Aqua BGM playing on each tabs and you cant shutdown the program unless you shutdown the computer.
+#Purpose: To make as many tabs as your friend's computer can handle with Aqua OP playing on each tabs and you cant shutdown the program unless you shutdown the computer.
 
 while True:
     #will loop for eternity until he/she presses Ctrl + C (which they cant because it will keep opening another tabs >:) )
